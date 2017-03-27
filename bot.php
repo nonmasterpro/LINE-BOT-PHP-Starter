fa4644fa3572e35e3d9382002ff3d8d1b059ab0d
teerapong <teerapong_boonmak@cmu.ac.th>
2017-03-27 14:25:45 +0700

    test4

--- bot.php
+++ bot.php
@@ -16,12 +16,12 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-      $userId = $event['userId'];
+      // $userId = $event['userId'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $userId
+				'text' => $replyToken
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
