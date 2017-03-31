bf5a53e93df27187a12e56e1c1c5a22de15475f0
teerapong <teerapong_boonmak@cmu.ac.th>
2017-03-31 10:50:34 +0700

    test11

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $replyToken
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -43,6 +43,38 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
       echo $result . "\r\n";
+
+		} else if($event['type'] == 'message' && $event['message']['type'] == 'sticker'){
+
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => "สติกเกอร์"
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+
 		}
 	}
 }
