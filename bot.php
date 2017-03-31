2bd3f6e3aad659d7bd9539ff052038ba02980bd8
teerapong <teerapong_boonmak@cmu.ac.th>
2017-03-31 11:38:43 +0700

    test

--- bot.php
+++ bot.php
@@ -83,31 +83,9 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				"type": "template",
-  "altText": "this is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-      "title": "Menu",
-      "text": "Please select",
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
-          }
-      ]
-  }
+				'type' => 'sticker',
+				'packageId' => '1',
+				'stickerId' => (rand(1,17))
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
