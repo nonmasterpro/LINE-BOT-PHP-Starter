a5777b459a8372629164aabe61717a8dc02ba068
teerapong <teerapong_boonmak@cmu.ac.th>
2017-03-31 16:25:30 +0700

    test

--- bot.php
+++ bot.php
@@ -147,8 +147,9 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => 'https://www.google.co.th'
+				"type": "image",
+    		"originalContentUrl": "https://s3-us-west-1.amazonaws.com/powr/defaults/image-slider2.jpg",
+    		"previewImageUrl": "https://s3-us-west-1.amazonaws.com/powr/defaults/image-slider2.jpg"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
