30e4984a3b1eb0b4bd4f4dd67038a6a6f2218c1f
teerapong <teerapong_boonmak@cmu.ac.th>
2017-03-31 10:57:24 +0700

    test12

--- bot.php
+++ bot.php
@@ -51,8 +51,9 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => "สติกเกอร์"
+				'type' => 'sticker',
+				'packageId' => '1',
+				'stickerId' => '1'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
