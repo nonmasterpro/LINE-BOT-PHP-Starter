dcc36572f0add7531dca9469ffab0d1f2c89d8f3
teerapong <teerapong_boonmak@cmu.ac.th>
2017-03-31 11:33:45 +0700

    test

--- bot.php
+++ bot.php
@@ -83,9 +83,30 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'sticker',
-				'packageId' => '1',
-				'stickerId' => (rand(1,17))
+				"type": "template",
+ 				"altText": "this is a buttons template",
+ 				"template": {
+		 		"type": "buttons",
+		 		"thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+		 		"title": "Menu",
+		 		"text": "Please select",
+		 		"actions": [
+				 {
+					 "type": "postback",
+					 "label": "Buy",
+					 "data": "action=buy&itemid=123"
+				 },
+				 {
+					 "type": "postback",
+					 "label": "Add to cart",
+					 "data": "action=add&itemid=123"
+				 },
+				 {
+					 "type": "uri",
+					 "label": "View detail",
+					 "uri": "http://www.google.co.th"
+				 }
+		 ]
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
