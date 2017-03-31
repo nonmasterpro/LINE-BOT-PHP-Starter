26ecb68fcae22dc0feb35882c5e1026bc04d20dc
teerapong <teerapong_boonmak@cmu.ac.th>
2017-03-31 11:37:20 +0700

    test

--- bot.php
+++ bot.php
@@ -84,29 +84,30 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				"type": "template",
- 				"altText": "this is a buttons template",
- 				"template": {
-		 		"type": "buttons",
-		 		"thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-		 		"title": "Menu",
-		 		"text": "Please select",
-		 		"actions": [
-
-					 "type": "postback",
-					 "label": "Buy",
-					 "data": "action=buy&itemid=123"
-				 ,
-
-					 "type": "postback",
-					 "label": "Add to cart",
-					 "data": "action=add&itemid=123"
-				 ,
-
-					 "type": "uri",
-					 "label": "View detail",
-					 "uri": "http://www.google.co.th"
-				 
-		 ]
+  "altText": "this is a buttons template",
+  "template": {
+      "type": "buttons",
+      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+      "title": "Menu",
+      "text": "Please select",
+      "actions": [
+          {
+            "type": "postback",
+            "label": "Buy",
+            "data": "action=buy&itemid=123"
+          },
+          {
+            "type": "postback",
+            "label": "Add to cart",
+            "data": "action=add&itemid=123"
+          },
+          {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+          }
+      ]
+  }
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
