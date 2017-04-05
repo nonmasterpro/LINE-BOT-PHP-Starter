78cd614614d8045ffb9e23b10ba754ffb41b801e
teerapong <teerapong_boonmak@cmu.ac.th>
2017-04-05 15:20:51 +0700

    test

--- bot.php
+++ bot.php
@@ -210,9 +210,9 @@ if (!is_null($events['events'])) {
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			// $url = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://api.line.me/v2/bot/message/reply';
 
-			$url = 'https://api.line.me/v2/bot/profile/'.$userId.'';
+			// $url = 'https://api.line.me/v2/bot/profile/'.$userId.'';
 
 
 			$data = [
