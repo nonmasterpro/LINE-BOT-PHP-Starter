29fb54d1b131e69ee8cccc570904b5740dc3cc71
teerapong <teerapong_boonmak@cmu.ac.th>
2017-04-10 17:09:05 +0700

    test10

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 
       $userId = $event['source']['userId'];
 
-			file_put_contents('test2.txt', $text,FILE_APPEND);
+			file_put_contents('test2.txt', { text : $text },FILE_APPEND);
 
 			// Build message to reply back
 			$messages = [
