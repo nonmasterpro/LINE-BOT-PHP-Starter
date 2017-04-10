517d695567700f9744ffc8a21b7a81bc9b0de63f
teerapong <teerapong_boonmak@cmu.ac.th>
2017-04-10 16:38:52 +0700

    testt

--- bot.php
+++ bot.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
-			file_put_contents('test.json', json_encode($text));
+			file_put_contents('test2.txt', json_encode($text));
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
