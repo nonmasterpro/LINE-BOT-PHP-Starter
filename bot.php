baf88f3d6db19059428340032750b02d895e2238
teerapong <teerapong_boonmak@cmu.ac.th>
2017-04-10 17:27:15 +0700

    test

--- bot.php
+++ bot.php
@@ -19,13 +19,14 @@ if (!is_null($events['events'])) {
 
       $userId = $event['source']['userId'];
 
-			// file_put_contents('test.json', json_encode($text),FILE_APPEND);
-			$data[] = {'text': $text};
-			$inp = file_get_contents('test.json');
-			$tempArray = json_decode($inp);
-			array_push($tempArray, $data);
-			$jsonData = json_encode($tempArray);
-			file_put_contents('test.json', $jsonData);
+			file_put_contents('test.json', json_encode($text),FILE_APPEND);
+			
+			// $data[] = {'text': $text};
+			// $inp = file_get_contents('test.json');
+			// $tempArray = json_decode($inp);
+			// array_push($tempArray, $data);
+			// $jsonData = json_encode($tempArray);
+			// file_put_contents('test.json', $jsonData);
 
 			// Build message to reply back
 			$messages = [
