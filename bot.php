13e48c4094fea9686d6844043345d1600420514e
teerapong <teerapong_boonmak@cmu.ac.th>
2017-04-20 11:19:33 +0700

    test

--- bot.php
+++ bot.php
@@ -260,7 +260,7 @@ function getMassage($text,$uid)
 			return "can't connect to db";
    }
 
-   $sql = "SELECT * FROM users ";
+   $sql = "SELECT * FROM users WHERE uid_line=$uid";
    if (mysqli_query($conn, $sql)) {
        $result = $conn->query($sql);
        return $result->num_rows;
