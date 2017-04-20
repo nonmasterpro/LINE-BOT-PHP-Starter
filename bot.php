060b4adbe7aed8a9ab4da0b1032c058e4d67b102
teerapong <teerapong_boonmak@cmu.ac.th>
2017-04-20 14:11:44 +0700

    test

--- bot.php
+++ bot.php
@@ -266,7 +266,7 @@ function getMassage($text,$uid)
 
    if (mysqli_query($conn, $key)) {
        $result = $conn->query($key);
-       return $result;
+       echo $result;
 			//  return $uid;
        // return “New record created successfully”;
    } else {
