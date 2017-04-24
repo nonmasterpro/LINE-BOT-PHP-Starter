118565c38eef9baf53fcace6efa43c0078841a86
teerapong <teerapong_boonmak@cmu.ac.th>
2017-04-24 17:55:45 +0700

    test

--- bot.php
+++ bot.php
@@ -329,12 +329,14 @@ function getMassage($text,$uid)
 		 $result2 = $conn->query($quotaK);
 		 $result3 = $conn->query($quotaS);
 		 $data= '';
+		 $data2= '';
+
 		 if ($result->num_rows > 0) {
 			 while($roww1 = $result3->fetch_assoc()) {
 				 $data['result1'] = $roww["quota"];
 			 }
 			 while($roww = $result2->fetch_assoc()) {
-				$data['result2'] = $roww["quota"];
+				$data2['result2'] = $roww["quota"];
 			}
 
 			 return "จำนวนวันลาป่วยของคุณเหลือ ".$data['result1']." วันค่ะ 😙 ".$data['result2'];
