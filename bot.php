0522d63b383d04ee5c68c158bdfaeba308d3876f
teerapong <teerapong_boonmak@cmu.ac.th>
2017-04-25 12:06:47 +0700

    test

--- bot.php
+++ bot.php
@@ -500,27 +500,27 @@ function getTemplate(){
 ;
 }
 
-function getConfirm(){
-	return [
-		"type"=> "template",
-  "altText"=> "this is a confirm template",
-  "template"=> [
-      "type"=> "confirm",
-      "text"=> "Are you sure?",
-      "actions"=> [
-          [
-            "type"=> "message",
-            "label"=> "Yes",
-            "text"=> "yes"
-          ],
-          [
-            "type"=> "message",
-            "label"=> "No",
-            "text"=> "no"
-          ]
-      ]
-  ]
-];
-}
+// function getConfirm(){
+// 	return [
+// 		"type"=> "template",
+//   "altText"=> "this is a confirm template",
+//   "template"=> [
+//       "type"=> "confirm",
+//       "text"=> "Are you sure?",
+//       "actions"=> [
+//           [
+//             "type"=> "message",
+//             "label"=> "Yes",
+//             "text"=> "yes"
+//           ],
+//           [
+//             "type"=> "message",
+//             "label"=> "No",
+//             "text"=> "no"
+//           ]
+//       ]
+//   ]
+// ];
+// }
 
 echo "OK";
