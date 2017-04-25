bfd55cb6dec42a07f4492bfd3da6841f8b806ad6
teerapong <teerapong_boonmak@cmu.ac.th>
2017-04-25 12:10:31 +0700

    test

--- bot.php
+++ bot.php
@@ -502,4 +502,27 @@ function getTemplate(){
 ;
 }
 
+function getConfirm(){
+	return [
+		"type"=> "template",
+  "altText"=> "this is a confirm template",
+  "template"=> [
+      "type"=> "confirm",
+      "text"=> "Are you sure?",
+      "actions"=> [
+          [
+            "type"=> "message",
+            "label"=> "Yes",
+            "text"=> "yes"
+          ],
+          [
+            "type"=> "message",
+            "label"=> "No",
+            "text"=> "no"
+          ]
+      ]
+  ]
+];
+}
+
 echo "OK";
