de03b27831bb77f88b8a6132ca722fc900005751
teerapong <teerapong_boonmak@cmu.ac.th>
2017-04-25 11:59:29 +0700

    test

--- bot.php
+++ bot.php
@@ -298,7 +298,6 @@ function getMassage($text,$uid)
 	 if($resultUser->num_rows > 0){
 		 while($rowuser = $resultUser->fetch_assoc()) {
 			 if($rowuser["status"]==0){
-				 return getTemplate();
 				//  if ($rowuser["id_card"] == null) {
 				// 	$sql 	= "UPDATE users SET id_card='".$text."' WHERE uid_line='".$uid."'";
 				// 	$result = $conn->query($sql);
@@ -307,11 +306,9 @@ function getMassage($text,$uid)
 				// $sql 	= "UPDATE users SET uid_line='$uid', status=0.5 WHERE id_card='".$text."' ";
 				// $result = $conn->query($sql);
 				// if($rowuser["name"] != null){
-
-					// $sql 	= "UPDATE users SET uid_line='$uid',status=1 WHERE id_card='".$text."'";
-					// $result = $conn->query($sql);
-					// return "สวัสดีค่ะ คุณ ".$rowuser["name"]." 😁";
-
+					$sql 	= "UPDATE users SET uid_line='$uid',status=1 WHERE id_card='".$text."'";
+					$result = $conn->query($sql);
+					return "สวัสดีค่ะ คุณ ".$rowuser["name"]." 😁";
 				// }
 
 			 }
