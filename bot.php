37258ac5fae27537665cd0e11c2b4b4a7b9f22ba
teerapong <teerapong_boonmak@cmu.ac.th>
2017-04-27 15:04:38 +0700

    test

--- bot.php
+++ bot.php
@@ -110,7 +110,7 @@ if (!is_null($events['events'])) {
 
 	}
 
-}else if(preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $text)){
+}else if('/\xEE[\x80-\xBF][\x80-\xBF]|\xEF[\x81-\x83][\x80-\xBF]/', $text)){
 	$messages = [
 		'type' => 'text',
 		'text' => "สัส😥 "
